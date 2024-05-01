50a74a1e6f87cac70b1dfeb7f27a92e82cd01bf1
mizech <michael.zech@hotmail.de>
2024-05-01 15:43:44 +0200

    Updated: seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Project.delete_all
+
+5.times { |i| Project.create(title: "Project-Title #{i}", description: "Project-Description #{i}") }
+
+
