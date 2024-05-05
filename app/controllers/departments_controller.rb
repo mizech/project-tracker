079f8704422f5edd686d29026925c8fcc2a118e8
mizech <michael.zech@hotmail.de>
2024-05-05 15:33:36 +0200

    departments_contr.: Added comment

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,4 +1,5 @@
 class DepartmentsController < ApplicationController
+  # <%= collection_select(:empleado, :departamento_id, Departamento.all, :id, :nombre_departamento, {:prompt => false}) %>
   def index
     @department = Department.all
   end
