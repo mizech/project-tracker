e0af6f70d70abd603d5e3d1f089e0d7393524bd6
mizech <michael.zech@hotmail.de>
2024-05-09 09:49:48 +0200

    Impl.: set_department

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,10 +1,16 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show]
+
   # <%= collection_select(:empleado, :departamento_id, Departamento.all, :id, :nombre_departamento, {:prompt => false}) %>
   def show
-    @department = Department.find(params[:id])
   end
 
   def index
     @departments = Department.all
+  end
+
+  private
+  def set_department
+    @department = Department.find(params[:description])
   end
 end
