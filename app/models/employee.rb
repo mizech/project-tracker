c720eda2f86c08da50cd80e4c416a2b32bcbde59
mizech <michael.zech@hotmail.de>
2024-05-12 13:10:38 +0200

    Created: ProjectAssignment

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ApplicationRecord
   belongs_to :department
+  has_many :project_assignments, dependent: :destroy
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
+  has_many :project_assignments, dependent: :destroy
+
   validates :title, presence: true, length: {minimum: 3}
 end
